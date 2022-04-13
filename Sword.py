4e95039736198cd40ddffde37ab07312c9beb5b6
Shawn-lung <72637628+Shawn-lung@users.noreply.github.com>
2022-04-14 00:37:30 +0800

    Update Sword.py

--- Sword.py
+++ Sword.py
@@ -1,9 +1,8 @@
 import pygame
-
+import os
 from Player import SPEED_LIMIT
 WINDOW_WIDTH = 800
 GROUND_LEVEL = 400
-
 class Sword(pygame.sprite.Sprite):
     def __init__(self,n):
         pygame.sprite.Sprite.__init__(self)
@@ -18,20 +17,18 @@ class Sword(pygame.sprite.Sprite):
         self.speedr = 0
         self.speedl = 0
         self.totaldegree = 0
+        self.direction = n
     def update(self, n):
-        '''if self.rotating:
-            self.rotate(-3)
-            self.degree -= 3
-            if self.degree == 0:
-                self.reverse = True
-                self.rotating = False'''
-        '''if self.reverse:
-            self.rotate(3)
-            self.degree += 3
-            if self.degree == 90:
-                self.reverse = False'''       
+        if self.rotating:
+            self.swing(self.direction)
+        if self.reverse:
+            self.reversing(self.direction)       
         key_pressed = pygame.key.get_pressed()
         self.rect.centerx += self.speedr - self.speedl
+        if self.speedr > self.speedl and not self.rotating and not self.reverse:
+            self.direction = 1
+        elif self.speedr < self.speedl and not self.rotating and not self.reverse:
+            self.direction = 2    
         if self.speedr != 0:
             self.speedr -= 0.5
         if self.speedl != 0:
@@ -41,25 +38,51 @@ class Sword(pygame.sprite.Sprite):
                 self.speedr += 1
             if key_pressed[pygame.K_LEFT] and self.speedl < SPEED_LIMIT:
                 self.speedl += 1
+            if self.rotating == False and self.reverse == False and key_pressed[pygame.K_SLASH]:
+                self.rotating = True
+        
         else:
             if key_pressed[pygame.K_d] and self.speedr < SPEED_LIMIT:
                 self.speedr += 1            
             if key_pressed[pygame.K_a] and self.speedl < SPEED_LIMIT:
                 self.speedl += 1 
-        if self.rotating == False and self.reverse == False and key_pressed[pygame.K_q]:
-            #self.rotating = True
-            self.rotate(1)     
+            if self.rotating == False and self.reverse == False and key_pressed[pygame.K_q]:
+                self.rotating = True    
         if self.rect.right > WINDOW_WIDTH:
             self.rect.right = WINDOW_WIDTH
         if self.rect.left < 0:
             self.rect.left = 0 
     def rotate(self,degree):
+        
         self.totaldegree+=degree
         self.totaldegree = self.totaldegree % 360
-        print(self.totaldegree)
         self.image = pygame.transform.rotate(self.ori_image , self.totaldegree)
         center = self.rect.center
         self.rect = self.image.get_rect()
         self.rect.center = center
+    def swing(self,n):    
+        if n == 2:
+            self.rotate(5)
+            self.degree += 5
+            if self.degree == 180:
+                self.reverse = True
+                self.rotating = False
+        else:
+            self.rotate(-5)
+            self.degree -= 5
+            if self.degree == 0:
+                self.reverse = True
+                self.rotating = False
+    def reversing(self,n):    
+        if n == 2:
+            self.rotate(-5)
+            self.degree -= 5
+            if self.degree == 90:
+                self.reverse = False
+        else:
+            self.rotate(5)
+            self.degree += 5
+            if self.degree == 90:
+                self.reverse = False
         
         
