cf73eff2405273ebebd717c8aa9417af4fd38ed6
shawn <you@shawnlung0429@gmail.com>
2022-04-14 11:38:47 +0800

    commits sword squat change

--- Sword.py
+++ Sword.py
@@ -18,11 +18,22 @@ class Sword(pygame.sprite.Sprite):
         self.speedl = 0
         self.totaldegree = 0
         self.direction = n
+        self.squat = False
+        self.squatr = False
+        self.done = False
     def update(self, n):
+        self.degree = self.degree%360
         if self.rotating:
             self.swing(self.direction)
         if self.reverse:
             self.reversing(self.direction)       
+        if self.squat:
+            self.squatting(self.direction)    
+        if self.squatr:
+            if self.degree != 90:
+                self.rsquatting(self.rdirection)
+            else:
+                self.squatr = False    
         key_pressed = pygame.key.get_pressed()
         self.rect.centerx += self.speedr - self.speedl
         if self.speedr > self.speedl and not self.rotating and not self.reverse:
@@ -33,6 +44,7 @@ class Sword(pygame.sprite.Sprite):
             self.speedr -= 0.5
         if self.speedl != 0:
             self.speedl -= 0.5
+        
         if n==2: 
             if key_pressed[pygame.K_RIGHT] and self.speedr < SPEED_LIMIT:
                 self.speedr += 1
@@ -40,7 +52,13 @@ class Sword(pygame.sprite.Sprite):
                 self.speedl += 1
             if self.rotating == False and self.reverse == False and key_pressed[pygame.K_SLASH]:
                 self.rotating = True
-        
+            if key_pressed[pygame.K_DOWN] and self.squatr == False:
+                self.squat = True
+            else:
+                if self.squat:
+                    self.rdirection = self.direction
+                    self.squat = False
+                    self.squatr = True
         else:
             if key_pressed[pygame.K_d] and self.speedr < SPEED_LIMIT:
                 self.speedr += 1            
@@ -48,6 +66,13 @@ class Sword(pygame.sprite.Sprite):
                 self.speedl += 1 
             if self.rotating == False and self.reverse == False and key_pressed[pygame.K_q]:
                 self.rotating = True    
+            if key_pressed[pygame.K_s] and self.squatr == False:
+                self.squat = True
+            else:
+                if self.squat:
+                    self.rdirection = self.direction
+                    self.squat = False
+                    self.squatr = True    
         if self.rect.right > WINDOW_WIDTH:
             self.rect.right = WINDOW_WIDTH
         if self.rect.left < 0:
@@ -84,5 +109,24 @@ class Sword(pygame.sprite.Sprite):
             self.degree += 5
             if self.degree == 90:
                 self.reverse = False
+    def squatting(self, n):
+            if n == 1:            
+                if self.degree != 310:
+                    self.rotate (-10)
+                    self.degree -= 10
+            else:            
+                if self.degree < 230:
+                    self.rotate (10)
+                    self.degree += 10
+    def rsquatting(self, n):
+            if n == 1:            
+                self.rotate (10)
+                self.degree += 10
+            else:            
+                self.rotate (-10)
+                self.degree -= 10                          
+                 
+                       
+
         
         
